cb8122d21b588e178904076c0d1541b686494733
Digudev <1.5.dg.diego@gmail.com>
2024-10-03 10:28:17 -0600

    Config: Analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 export const metadata = {
@@ -7,8 +8,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html lang="es">
+			<body>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
