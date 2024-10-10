21bd85a96e90b33ed031467838c6372a4cc91f10
Digudev <1.5.dg.diego@gmail.com>
2024-10-10 16:55:50 -0600

    Styles: Gradients added

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className={poppins.className}>
-			<body className="flex items-center justify-center">
+			<body className="flex items-center justify-center w-screen h-screen">
 				{children}
 				<Analytics />
 			</body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,26 @@
 import Image from 'next/image';
-import Playlist from '@/data/playlist';
-import Social from '@/data/social';
+import Social from './components/Social';
+import Carousel from './components/Carousel';
 
 export default function Home() {
 	return (
-		<div className="flex flex-col w-screen h-screen items-center justify-start gap-10 p-[50px] text-[#E9E9E9] md:w-[1000px]">
-			<div className="flex flex-col items-center justify-center gap-5 mt-[10%]">
-				<Image
-					className="rounded-full border border-white"
-					src={'/assets/logo.jpg'}
-					alt="Noisy Logo"
-					width={100}
-					height={100}
-				/>
-				<h1 className="text-4xl font-bold">xNoisy</h1>
-				<div className="flex flex-row gap-2 w-full h-fit justify-center">
-					{Social.map((link) => (
-						<a key={link.title} className=" overflow-hidden" href={link.url} target="blank">
-							<div className="size-[40px]">{link.icon}</div>
-						</a>
-					))}
+		<>
+			<div className="absolute -z-50 overflow-hidden w-screen h-screen">
+				<div className="blue-gradient"></div>
+				<div className="orange-gradient"></div>
+				<div className="pink-gradient"></div>
+			</div>
+			<div className="flex flex-col w-screen h-screen items-center justify-start gap-10 p-[50px] text-[#E9E9E9] md:w-[1000px] overflow-hidden ">
+				<div className="flex flex-col items-center justify-center gap-5 mt-[10%]">
+					<Image src={'/assets/logo.png'} alt="Noisy Logo" width={80} height={80} />
+					<h1 className="text-4xl font-bold">xNoisy</h1>
+					<Social />
+				</div>
+				<div className="flex flex-col w-full gap-4 items-center">
+					<h2 className="text-2xl font-bold">Playlist</h2>
+					<Carousel />
 				</div>
 			</div>
-			<div className="flex flex-col w-full gap-4 items-center">
-				<h2 className="text-2xl">Playlist ⇣</h2>
-				{Playlist.map((link) => (
-					<a
-						key={link.title}
-						className="font-medium flex border border-2 border-white rounded-2xl w-full h-fit p-3 items-center justify-center bg-[#ffffff14] text-xl"
-						href={link.url}
-						target="blank"
-					>
-						<Image src={link.img} width={40} height={40} className="mr-3 rounded-lg" />
-						{link.title}
-					</a>
-				))}
-			</div>
-		</div>
+		</>
 	);
 }
